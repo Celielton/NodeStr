0c9acdedcd4265ab1b1f17196c8663b41b841ce8
Celielton Ramalho <celton1712@hotmail.com>
2019-03-23 12:49:41 -0300

    config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = '02C4C1ACB51D60CE8FB0691F106289A045E3F2B229124E418BC315206CFF3FA7';
+global.EMAIL_TEMPLATE = '<strong>{0}</strong>';
+
+
+module.exports = {
+    //connectionString: "mongodb://sa:sa123$@localhost:27017/NodeStr",
+    connectionString: 'mongodb://localhost:27017/NodeStr'
+}
